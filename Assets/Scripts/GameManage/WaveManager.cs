6daab05ca9c463de1dadb0dd857df4b2f846c65d
h1snysu <145316696+h1snysu@users.noreply.github.com>
2024-10-31 20:35:04 +0800

    [add] add battlescene ground grid animation for the first 3 waves

--- Assets/Scripts/GameManage/WaveManager.cs
+++ Assets/Scripts/GameManage/WaveManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveManager : MonoBehaviour
 {
@@ -17,8 +18,21 @@ public class WaveManager : MonoBehaviour
     [SerializeField]
     private float intervalWave;
 
+    [SerializeField]
+    private float animationDuration = 1f;
+
+    [SerializeField]
+    private Image oneRow;
+
+    [SerializeField]
+    private Image threeRow;
+
     private GameObject ZombieSpawner;
     private GameObject GridManager;
+
+    private bool isAnimating;
+    private float animTimer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +40,10 @@ public class WaveManager : MonoBehaviour
         GridManager = GameObject.Find("GridManager");
         intervalWave = 0;
 
-        if (ZombieSpawner == null || GridManager == null) 
+        animTimer = 0f;
+        isAnimating = false;
+
+        if (ZombieSpawner == null || GridManager == null)
         {
             Debug.Log("ERROR in WVMG");
         }
@@ -35,16 +52,35 @@ public class WaveManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (initInterval > 0)            // set a blank reation time for player
+        if (isAnimating)
         {
-            initInterval -= Time.deltaTime;
-
+            if (currentWaveNum == 2)
+            {
+                fillRoad(oneRow);
+            }
+            else if (currentWaveNum == 3)
+            {
+                fillRoad(threeRow);
+            }
             return;
         }
 
-        if(intervalWave <= 0)
+        if (initInterval > 0)            // set a blank reation time for player
+        {
+            initInterval -= Time.deltaTime;
+            return;
+        }
+
+        if (intervalWave <= 0)
         {
             currentWaveNum++;
+
+            if (currentWaveNum == 2 || currentWaveNum == 3)
+            {
+                animTimer = 0f;
+                isAnimating = true;
+            }
+
             ZombieSpawner.GetComponent<ZombieSpawner>().spawnWave(currentWaveNum);
             GridManager.GetComponent<GridManager>().setWave(currentWaveNum);
 
@@ -55,5 +91,22 @@ public class WaveManager : MonoBehaviour
             intervalWave -= Time.deltaTime;
         }
 
+    }
+
+
+    private void fillRoad(Image image)
+    {
+        animTimer += Time.deltaTime;
+
+        image.fillAmount = Mathf.Lerp(1f, 0f, animTimer / animationDuration);
+
+        if (image.fillAmount == 0f)
+        {
+            image.fillAmount = 0f;
+            isAnimating = false;
+            
+            ZombieSpawner.GetComponent<ZombieSpawner>().spawnWave(currentWaveNum);
+            GridManager.GetComponent<GridManager>().setWave(currentWaveNum);
+        }
     }
 }
