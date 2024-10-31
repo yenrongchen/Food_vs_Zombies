8afe4f5df7dd3d07b33bfc896d661effa2bf97e5
h1snysu <145316696+h1snysu@users.noreply.github.com>
2024-10-31 12:43:23 +0800

    [fix] Stop Player movement when camera is in BattleScene

--- Assets/Scripts/CookingScene/PlayerController.cs
+++ Assets/Scripts/CookingScene/PlayerController.cs
@@ -42,37 +42,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // player movement
-        directionX = Input.GetAxisRaw("Horizontal");
-        directionY = Input.GetAxisRaw("Vertical");
-        rbody2D.velocity = new Vector2(directionX * walkSpeed, directionY * walkSpeed);
+        if (atMain)
+        {
+            // player movement
+            directionX = Input.GetAxisRaw("Horizontal");
+            directionY = Input.GetAxisRaw("Vertical");
+            rbody2D.velocity = new Vector2(directionX * walkSpeed, directionY * walkSpeed);
 
-        // reflect the sprites when switching direction
-        if (directionX * this.transform.localScale.x > 0)
-        {
-            this.transform.localScale = new Vector3(-1 * this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
-        }
+            // reflect the sprites when switching direction
+            if (directionX * this.transform.localScale.x > 0)
+            {
+                this.transform.localScale = new Vector3(-1 * this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+            }
 
-        // player animation
-        if (isTaking == false && (directionX != 0 || directionY != 0))  // turn to walk
-        {
-            animator.SetInteger("PlayerState", 1);
-        }
-        else if (isTaking == true && directionX == 0 && directionY == 0)  // turn to take
-        {
-            animator.SetInteger("PlayerState", 2);
-        }
-        else if (isTaking == true && (directionX != 0 || directionY != 0))  // turn to take & walk
-        {
-            animator.SetInteger("PlayerState", 3);
-        }
-        else                                                                // turn to stand
-        {
-            animator.SetInteger("PlayerState", 0);
-        }
+            // player animation
+            if (isTaking == false && (directionX != 0 || directionY != 0))  // turn to walk
+            {
+                animator.SetInteger("PlayerState", 1);
+            }
+            else if (isTaking == true && directionX == 0 && directionY == 0)  // turn to take
+            {
+                animator.SetInteger("PlayerState", 2);
+            }
+            else if (isTaking == true && (directionX != 0 || directionY != 0))  // turn to take & walk
+            {
+                animator.SetInteger("PlayerState", 3);
+            }
+            else                                                                // turn to stand
+            {
+                animator.SetInteger("PlayerState", 0);
+            }
 
-        // make sure player dont tilt because of rbody2D
-        this.transform.rotation = Quaternion.Euler(0, 0, 0);
+            // make sure player dont tilt because of rbody2D
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
 
         if (Input.GetKeyDown(KeyCode.Tab))
         {
